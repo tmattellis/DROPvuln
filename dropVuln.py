ad5e4e64a685236e9e3eca403624a5efdacc3474
tmattellis <t.ellis897@gmail.com>
2019-04-09 13:29:32 -0500

    Update dropVuln.py

--- dropVuln.py
+++ dropVuln.py
@@ -2,7 +2,7 @@ from joern.all import JoernSteps
 
 
 
-def runQuery(query, type):
+def runQuery(query, type="Placeholder"):
 
     results = j.runGremlinQuery(query)
 
@@ -17,6 +17,90 @@ def main():
 
     j.connectToDatabase()
 
+    queries = {
+    "Buffer Overflow 1" : '''
+                            getFunctionASTsByName('*_write*')
+                            .getArguments('(copy_from_user OR memcpy)', '2')
+                            .sideEffect{ paramName = 'c(ou)?nt';}
+                            .filter{ it.code.matches(paramName) }
+                            .unsanitized(
+                                { it._().or(
+                                _().isCheck('.*' + paramName + '.*'),
+                                _().codeContains('.*alloc.*' + paramName + '.*'),
+                                _().codeContains('.*min.*')
+                                )}
+                            )  
+                            .param('.*c(ou)?nt')
+                            .locations()
+                          ''',
+
+    "Buffer Overflow 2" : '''
+                            getArguments('(copy_from_user OR memcpy)', '2')
+                            .filter{ !it.argToCall().toList()[0].code.matches('.*(sizeof|min).*') }
+                            .sideEffect{ argument = it.code; } 
+                            .sideEffect{ dstld = it.statements().toList()[0].id; } 
+                            .unsanitized({
+                                it._().or(
+                                  _().isCheck('.*' + Pattern.quote(argument) + '.*'),
+                                  _().codeContains('.*alloc.*' + Pattern.quote(argument) + '.*'),
+                                  _().codeContains('.*min.*')
+                                )
+                            }, {it._().filter{ it.code.contains('copy_from_user')} }
+                            )
+                            .filter{ it.id != dstId }
+                            .locations()
+                          ''',
+
+    "Code Injection" : '''
+                            getArguments('recv', '1')
+                            .sideEffect{ paramName = it.code;}
+                            .filter{ it.code.matches(paramName) }
+                            .filter{ it.code.matches('system') }
+                            .unsanitized(
+                                { it._().or(
+                                  _().isCheck('.*' + paramName + '.*'),
+                                  _().codeContains('.*;.*')
+                                  )}
+                            )
+                            .locations()
+                       ''',
+
+    "Insecure Arguments" : '''
+                            getCallsTo('.*printf.*').ithArguments('1')
+                            .sideEffect{ param = it.code }
+                            .match{ it.type != "const String" }
+                            .locations()
+                           ''',
+
+    "Integer Overflow" : '''
+                            getCallsTo('malloc').ithArguments('0')
+                            .sideEffect{ param = it.code }
+                            .match{ it.type == "AdditiveExpression" }.statements()
+                            .out("REACHES")
+                            .match{ it.type == "CallExpression" && it.code.startsWith("memcpy") }.ithArguments("2")
+                            .filter{ it.code != param}
+                            .match{ it.type != "AdditiveExpression" }
+                            .locations()
+                         ''',
+
+    "Zero-Byte Allocation" : '''
+                            getArguments('.*alloc.*', '0')
+                            .sideEffect{ paramName = it.code;}
+                            .filter{ it.code.matches(paramName) }
+                            .unsanitized(
+                                { it._().or(
+                                  _().isCheck('.*' + paramName + '.*'),
+                                  _().codeContains('.*0.*')
+                                  )}
+                            )
+                            .locations()
+                             '''
+    }
+
+    for query, type in queries:
+        runQuery(query)
+
+    # didn't copy in divide by zero given that it was written differently. 
 
 
 
